624df3abe58e0752062ea6cbf492bfd9128b7ec0
Roushan <kumar7roushanjha2005@gmail.com>
2025-02-22 17:51:14 +0530

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+// import jsdoc from 'eslint-plugin-jsdoc';
+import eslintPluginPrettierRecommended from 'eslint-config-prettier';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  // {
+  //   plugins: {
+  //     jsdoc: jsdoc,
+  //   },
+  // },
+  { languageOptions: { globals: globals.node } },
+  {
+    rules: {
+      eqeqeq: 'off',
+      'no-unused-vars': 'error',
+      'prefer-const': ['error', { ignoreReadBeforeAssign: true }],
+    },
+  },
+  { ignores: ['.node_modules/*',"dist/*"] },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
+
+export {
+  // Any other config imports go at the top
+  eslintPluginPrettierRecommended,
+};
---
+++ src/bot.ts
@@ -0,0 +1,28 @@
+import { Client, GatewayIntentBits } from 'discord.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.once('ready', () => {
+  if (client.user) {
+    console.log(`🤖 Bot is online as ${client.user.tag}`);
+  } else {
+    console.log('🤖 Bot is online, but user is not available.');
+  }
+});
+
+client.on('messageCreate', (message) => {
+  if (message.content === '!ping') {
+    message.reply('Pong! 🏓');
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
