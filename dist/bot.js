7e2e75f58a3ab86f7b40531dce6c5f04a44a5285
Roushan <kumar7roushanjha2005@gmail.com>
2025-02-22 18:36:00 +0530

    Update development scripts and enhance TypeScript configuration; modify bot behavior to respond to '!ping'

--- dist/bot.js
+++ dist/bot.js
@@ -2,24 +2,23 @@ import { Client, GatewayIntentBits } from 'discord.js';
 import dotenv from 'dotenv';
 dotenv.config();
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-  ],
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
 });
 client.once('ready', () => {
-  if (client.user) {
-    console.log(`🤖 Bot is online as ${client.user.tag}`);
-  } else {
-    console.log('🤖 Bot is online, but user is not available.');
-  }
+    if (client.user) {
+        console.log(`🤖 Bot is online as ${client.user.tag}`);
+    }
+    else {
+        console.log('🤖 Bot is online, but user is not available.');
+    }
 });
 client.on('messageCreate', (message) => {
-  if (message.content.startsWith('hi ')) {
-    message.reply(
-      `welcome ${message.author.globalName}, hope you found your piza`
-    );
-  }
+    if (message.content === '!ping') {
+        message.reply('Pong! 🏓');
+    }
 });
 client.login(process.env.DISCORD_TOKEN);
