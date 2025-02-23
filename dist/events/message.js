ce51d6479af56449f2436df46876983d95c5101f
Roushan <kumar7roushanjha2005@gmail.com>
2025-02-23 12:33:47 +0530

    feat: add joke functionality to welcome message in bot

--- dist/events/message.js
+++ dist/events/message.js
@@ -1,7 +1,22 @@
+import axios from 'axios';
+const userjoke = async () => {
+    try {
+        const res = await axios.get('https://v2.jokeapi.dev/joke/Programming');
+        // console.log(res);
+        return res.data.joke
+            ? res.data.joke
+            : `setup: ${res.data.setup} \n delivery:${res.data.delivery}`;
+    }
+    catch (error) {
+        console.error(error);
+        return 'I think you have done somthing 🤞 naughty';
+    }
+};
 export const handelClientMsg = async (message) => {
     if (message.author.bot)
         return;
-    if (message.content.startsWith("hi ")) {
-        message.reply(`👋 Welcome ${message.author.globalName}, hope you take your notes!`);
+    if (message.content.startsWith('hi ')) {
+        const tip = await userjoke();
+        message.reply(`👋 Welcome ${message.author.globalName}, \n Bot Gess a joke for you: \n ${tip}`);
     }
 };
--- src/events/message.ts
+++ src/events/message.ts
@@ -1,11 +1,25 @@
-import { Message } from "discord.js";
+import axios from 'axios';
+import { Message } from 'discord.js';
 
+const userjoke = async () => {
+  try {
+    const res = await axios.get('https://v2.jokeapi.dev/joke/Programming');
+    // console.log(res);
+    return res.data.joke
+      ? res.data.joke
+      : `setup: ${res.data.setup} \n delivery:${res.data.delivery}`;
+  } catch (error) {
+    console.error(error);
+    return 'I think you have done somthing 🤞 naughty';
+  }
+};
 export const handelClientMsg = async (message: Message<boolean>) => {
   if (message.author.bot) return;
-  
-  if (message.content.startsWith("hi ")) {
+
+  if (message.content.startsWith('hi ')) {
+    const tip = await userjoke();
     message.reply(
-      `👋 Welcome ${message.author.globalName}, hope you take your notes!`
+      `👋 Welcome ${message.author.globalName}, \n Bot Gess a joke for you: \n ${tip}`
     );
   }
 };
