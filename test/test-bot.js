f86d01f0317af849b2f44fef81d2e1c8ade3a97f
Roushan <kumar7roushanjha2005@gmail.com>
2025-02-23 11:50:49 +0530

    update environment variables in test-bot.js for additional configuration

--- test/test-bot.js
+++ test/test-bot.js
@@ -7,7 +7,12 @@ dotenv.config({
 });
 
 const envVariables = {
-  TOKEN: process.env.DISCORD_TOKEN,
+  DISCORD_TOKEN: process.env.DISCORD_TOKEN,
+  DISCORD_TOKEN: process.env.DISCORD_TOKEN,
+  APP_ID: process.env.APP_ID,
+  PUBLIC_KEY: process.env.PUBLIC_KEY,
+  IPINFO_TOKEN: process.env.IPINFO_TOKEN,
+  CLIENT_ID: process.env.CLIENT_ID,
 };
 
 Object.freeze(envVariables);
